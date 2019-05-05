42a8a5274c2668da78a976ad349df9daa3ba0011
William Candillon <wcandillon@gmail.com>
2019-05-05 14:39:05 +0200

    feat(📦): Add runDecay, decay, and preserveOffset to animation runners
    Thank you @dsznajder for such a great contribution 🙋🏼‍♂️

---
+++ Examples/preserveOffset.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Animated from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { preserveOffset } from "react-native-redash";
+
+const { Value, event } = Animated;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.X = new Value(0);
+    this.Y = new Value(0);
+    const dragX = new Value(0);
+    const dragY = new Value(0);
+    const panState = new Value(0);
+
+    this.handlePan = event([
+      {
+        nativeEvent: {
+          translationX: dragX,
+          translationY: dragY,
+          state: panState,
+        },
+      },
+    ]);
+
+    this.X = preserveOffset(dragX, panState);
+    this.Y = preserveOffset(dragY, panState);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PanGestureHandler
+          onGestureEvent={this.handlePan}
+          onHandlerStateChange={this.handlePan}
+        >
+          <Animated.View
+            style={[
+              styles.box,
+              {
+                transform: [{ translateX: this.X }, { translateY: this.Y }],
+              },
+            ]}
+          />
+        </PanGestureHandler>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  box: {
+    width: 40,
+    height: 40,
+    backgroundColor: "red",
+  },
+});
