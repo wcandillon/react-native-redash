b5d2db98061495805e52cf0b26baef80998d1d36
William Candillon <wcandillon@gmail.com>
2019-05-08 15:09:53 +0200

    fix(📦): Make runSpring configurable (#24)

--- src/AnimationRunners.ts
+++ src/AnimationRunners.ts
@@ -15,8 +15,11 @@ const {
   add,
 } = Animated;
 
-export { timing, clockRunning, add };
+export {
+  timing, spring, clockRunning, add,
+};
 
+export type SpringConfig= Parameters<typeof spring>[1];
 export type TimingConfig = Parameters<typeof timing>[1];
 export type Clock = Parameters<typeof clockRunning>[0];
 export type Node = ReturnType<typeof add>;
@@ -60,6 +63,15 @@ export function runSpring(
   clock: Clock,
   value: Adaptable<number>,
   dest: Adaptable<number>,
+  config: SpringConfig = {
+    toValue: new Value(0),
+    damping: 7,
+    mass: 1,
+    stiffness: 121.6,
+    overshootClamping: false,
+    restSpeedThreshold: 0.001,
+    restDisplacementThreshold: 0.001,
+  },
 ) {
   const state = {
     finished: new Value(0),
@@ -68,23 +80,13 @@ export function runSpring(
     time: new Value(0),
   };
 
-  const config = {
-    toValue: new Value(0),
-    damping: 7,
-    mass: 1,
-    stiffness: 121.6,
-    overshootClamping: false,
-    restSpeedThreshold: 0.001,
-    restDisplacementThreshold: 0.001,
-  };
-
   return block([
     cond(clockRunning(clock), 0, [
       set(state.finished, 0),
       set(state.time, 0),
       set(state.position, value),
       set(state.velocity, 0),
-      set(config.toValue, dest),
+      set(config.toValue as any, dest),
       startClock(clock),
     ]),
     spring(clock, state, config),
