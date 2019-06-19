4cd5a112528bed50e1d5cbf72c9dca353f82c170
William Candillon <wcandillon@gmail.com>
2019-06-19 18:00:35 +0200

    feat(🧮): Add asin()

--- src/Math.ts
+++ src/Math.ts
@@ -15,6 +15,7 @@ const {
   min: min2,
   max: max2,
   greaterOrEq,
+  greaterThan,
   pow
 } = Animated;
 
@@ -36,28 +37,40 @@ export const toRad = (deg: Animated.Adaptable<number>): Animated.Node<number> =>
 export const toDeg = (rad: Animated.Adaptable<number>): Animated.Node<number> =>
   multiply(rad, 180 / Math.PI);
 
-export const atan = (rad: Animated.Adaptable<number>): Animated.Node<number> =>
-  sub(
-    multiply(Math.PI / 4, rad),
-    multiply(
-      multiply(rad, sub(abs(rad), 1)),
-      add(0.2447, multiply(0.0663, abs(rad)))
-    )
-  );
+// https://developer.download.nvidia.com/cg/atan.html
+export const atan = (x: Animated.Adaptable<number>): Animated.Node<number> =>
+  atan2(x, 1);
 
+// https://developer.download.nvidia.com/cg/atan2.html
 export const atan2 = (
   y: Animated.Adaptable<number>,
   x: Animated.Adaptable<number>
 ): Animated.Node<number> => {
-  const coeff1 = Math.PI / 4;
-  const coeff2 = 3 * coeff1;
-  const absY = abs(y);
-  const angle = cond(
-    greaterOrEq(x, 0),
-    [sub(coeff1, multiply(coeff1, divide(sub(x, absY), add(x, absY))))],
-    [sub(coeff2, multiply(coeff1, divide(add(x, absY), sub(absY, x))))]
-  );
-  return cond(lessThan(y, 0), multiply(angle, -1), angle);
+  const t0: Animated.Value<number> = new Value();
+  const t1: Animated.Value<number> = new Value();
+  // const t2: Animated.Value<number> = new Value();
+  const t3: Animated.Value<number> = new Value();
+  const t4: Animated.Value<number> = new Value();
+  return block([
+    set(t3, abs(x)),
+    set(t1, abs(y)),
+    set(t0, max(t3, t1)),
+    set(t1, min(t3, t1)),
+    set(t3, divide(1, t0)),
+    set(t3, multiply(t1, t3)),
+    set(t4, multiply(t3, t3)),
+    set(t0, -0.01348047),
+    set(t0, add(multiply(t0, t4), 0.057477314)),
+    set(t0, sub(multiply(t0, t4), 0.121239071)),
+    set(t0, add(multiply(t0, t4), 0.195635925)),
+    set(t0, sub(multiply(t0, t4), 0.332994597)),
+    set(t0, add(multiply(t0, t4), 0.99999563)),
+    set(t3, multiply(t0, t3)),
+    set(t3, cond(greaterThan(abs(y), abs(x)), sub(1.570796327, t3), t3)),
+    set(t3, cond(lessThan(x, 0), sub(Math.PI, t3), t3)),
+    set(t3, cond(lessThan(y, 0), multiply(t3, -1), t3)),
+    t3
+  ]);
 };
 
 // https://developer.download.nvidia.com/cg/acos.html
@@ -78,6 +91,26 @@ export const acos = (x1: Animated.Adaptable<number>) => {
     set(ret, sqrt(sub(1, x))),
     set(ret, sub(ret, multiply(2, negate, ret))),
     add(multiply(negate, Math.PI), ret)
+  ]);
+};
+
+// https://developer.download.nvidia.com/cg/asin.html
+export const asin = (x1: Animated.Adaptable<number>) => {
+  const negate: Animated.Value<number> = new Value();
+  const x: Animated.Value<number> = new Value();
+  const ret: Animated.Value<number> = new Value();
+  return block([
+    set(negate, lessThan(x, 0)),
+    set(x, abs(x1)),
+    set(ret, -0.0187293),
+    set(ret, multiply(ret, x)),
+    set(ret, add(ret, 0.074261)),
+    set(ret, multiply(ret, x)),
+    set(ret, sub(ret, 0.2121144)),
+    set(ret, multiply(ret, x)),
+    set(ret, add(ret, 1.5707288)),
+    set(ret, sub(Math.PI / 2, multiply(sqrt(sub(1, x)), ret))),
+    sub(ret, multiply(2, negate, ret))
   ]);
 };
 
