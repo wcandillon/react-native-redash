8dddc0e6b9e08dd23bfb69f1be6e9eb1caaac531
Damian Sznajder <damsznajder@gmail.com>
2019-11-03 14:47:54 +0100

    withOffset

--- Example/src/Gestures/withOffset.tsx
+++ Example/src/Gestures/withOffset.tsx
@@ -1,16 +1,15 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { View, Text } from 'react-native'
 import { withOffset } from 'react-native-redash'
 import { PanGestureHandler, State as GestureState } from 'react-native-gesture-handler'
 import Animated from 'react-native-reanimated'
-import { useMemoOne } from 'use-memo-one'
 
 import Box from './Box'
 
 const { Value, event } = Animated
 
 const withOffsetExample = () => {
-  const { dragX, dragY, panState, translateY, translateX } = useMemoOne(() => {
+  const { dragX, dragY, panState, translateY, translateX } = useState(() => {
     const dragX = new Value(0)
     const dragY = new Value(0)
     const panState = new Value(GestureState.UNDETERMINED)
@@ -22,7 +21,7 @@ const withOffsetExample = () => {
       translateX: withOffset(dragX, panState),
       translateY: withOffset(dragY, panState)
     }
-  }, [])
+  })
 
   const onGestureEvent = useRef(event([({
     nativeEvent: {
