f6aa165ba4225c2d7b857f914559ebd1912986a1
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2020-01-10 08:49:01 -0300

    Enable SEO metas

--- packages/docs/src/components/SEO.tsx
+++ packages/docs/src/components/SEO.tsx
@@ -33,40 +33,41 @@ const SEO: React.FC<Props> = ({ description, lang, meta, title }) => {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      // meta={[
-      //   {
-      //     name: `description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     property: `og:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     property: `og:description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     property: `og:type`,
-      //     content: `website`,
-      //   },
-      //   {
-      //     name: `twitter:card`,
-      //     content: `summary`,
-      //   },
-      //   {
-      //     name: `twitter:creator`,
-      //     content: site.siteMetadata.author,
-      //   },
-      //   {
-      //     name: `twitter:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     name: `twitter:description`,
-      //     content: metaDescription,
-      //   },
-      // ].concat(meta)}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+        ...(meta || []),
+      ]}
     />
   )
 }
