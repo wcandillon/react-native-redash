c298bd636887bdb985e8c7a1d7e076b6b8584c45
William Candillon <wcandillon@gmail.com>
2020-01-26 16:55:13 +0100

    🔧

---
+++ docs/component---src-pages-404-js-efaf821ac349e9a3a3b7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("xH0s");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-efaf821ac349e9a3a3b7.js.map
--- packages/docs/doczrc.js
+++ packages/docs/doczrc.js
@@ -1,3 +1,4 @@
 export default {
   typescript: true,
-}
+  dest: "../../docs"
+};
