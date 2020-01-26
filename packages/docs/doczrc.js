c64cca218eff9ff86dd326d6e129e3e694a76ccb
William Candillon <wcandillon@gmail.com>
2020-01-26 17:32:02 +0100

    📚 (#167)

--- packages/docs/doczrc.js
+++ packages/docs/doczrc.js
@@ -1,5 +1,7 @@
 export default {
   typescript: true,
+  title: "React Native Redash",
+  description: "React Native Redash Documentation",
   dest: "../../docs",
   base: "/react-native-redash/"
 };
