709aa2dce9f1f6237882adbe02e19c0d09da99d1
William Candillon <wcandillon@gmail.com>
2020-04-05 21:45:27 +0200

    feat(🎨): colorForBackground() (#203)

--- packages/core/src/Colors.ts
+++ packages/core/src/Colors.ts
@@ -13,6 +13,8 @@ const {
   proc,
   color,
   Extrapolate,
+  greaterThan,
+  cond,
 } = Animated;
 
 type Color = Animated.Adaptable<string> | Animated.Adaptable<number>;
@@ -61,6 +63,17 @@ export const hsv2color = proc(
   ) => {
     const { r, g, b } = hsv2rgb(h, s, v);
     return color(r, g, b);
+  }
+);
+
+export const colorForBackground = proc(
+  (
+    r: Animated.Adaptable<number>,
+    g: Animated.Adaptable<number>,
+    b: Animated.Adaptable<number>
+  ) => {
+    const L = add(multiply(0.299, r), multiply(0.587, g), multiply(0.114, b));
+    return cond(greaterThan(L, 186), color(0, 0, 0), color(255, 255, 255));
   }
 );
 
