a62673fed6b173478283a2873bba2962843b204b
William Candillon <wcandillon@gmail.com>
2020-04-13 10:02:49 +0200

    fix(🧮): accept scalar values in vector functions

--- packages/core/src/__tests__/Vectors.test.ts
+++ packages/core/src/__tests__/Vectors.test.ts
@@ -27,6 +27,7 @@ test("multiply", () => {
   const d = vec.create(2, 2);
   const result = vec.createValue(2, 2);
   expect(vec.multiply(a, b, c, d)).toEqual(result);
+  expect(vec.multiply(2, 1, 1, 1)).toEqual(result);
 });
 
 test("divide", () => {
@@ -50,6 +51,20 @@ test("invert", () => {
 test("clamp", () => {
   const v1 = vec.create(-1, -1);
   const v2 = vec.create(2, 2);
+  const v3 = vec.create(0.3, 0.2);
+  const min = vec.create(0, 0);
+  const max = vec.create(1, 1);
+  const r1 = vec.createValue(0, 0);
+  const r2 = vec.createValue(1, 1);
+  const r3 = vec.createValue(0.3, 0.2);
+  expect(vec.clamp(v1, min, max)).toEqual(r1);
+  expect(vec.clamp(v2, min, max)).toEqual(r2);
+  expect(vec.clamp(v3, min, max)).toEqual(r3);
+});
+
+test("single value", () => {
+  const v1 = -1;
+  const v2 = 2;
   const v3 = vec.create(0.3, 0.2);
   const min = vec.create(0, 0);
   const max = vec.create(1, 1);
