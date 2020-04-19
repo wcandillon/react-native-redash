05a06628d7d1a44ac7e8b1af4198ac2925caf056
William Candillon <wcandillon@gmail.com>
2020-04-19 08:29:40 +0200

    feat(🎣): add useGestureHandler(), usePanGestureHandler(), useVerticalPanGestureHandler(), and useHorizontalPanGestureHandler()

--- packages/core/src/Gesture.ts
+++ packages/core/src/Gesture.ts
@@ -12,6 +12,7 @@ import {
 } from "react-native-gesture-handler";
 
 import { Platform } from "react-native";
+import { useMemoOne } from "use-memo-one";
 import { snapPoint } from "./Animations";
 
 const {
@@ -335,3 +336,24 @@ export const debugGestureState = (
     )
   );
 };
+
+type Dependencies = readonly unknown[];
+
+export const useGestureHandler = (
+  nativeEvent: Partial<Adaptable<NativeEvent>>,
+  deps: Dependencies
+) =>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useMemoOne(() => onGestureEvent(nativeEvent), deps);
+
+export const usePanGestureHandler = (deps: Dependencies) =>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useMemoOne(() => panGestureHandler(), deps);
+
+export const useVerticalPanGestureHandler = (deps: Dependencies) =>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useMemoOne(() => verticalPanGestureHandler(), deps);
+
+export const useHorizontalPanGestureHandler = (deps: Dependencies) =>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useMemoOne(() => horizontalPanGestureHandler(), deps);
