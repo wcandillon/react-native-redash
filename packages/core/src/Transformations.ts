5e0b7c3523ea3b7b6df3aaa50928289e8681f73f
William Candillon <wcandillon@gmail.com>
2020-04-24 10:08:37 +0200

    feat(🚤): Faster 2d operations (#242)

--- packages/core/src/Transformations.ts
+++ packages/core/src/Transformations.ts
@@ -1,9 +1,9 @@
 import Animated from "react-native-reanimated";
 
-import { Vector } from "./Vectors";
+import { Vector, vec } from "./Vectors";
 import { Transforms } from "./Matrix";
 
-const { divide, sub, multiply } = Animated;
+const { divide, sub, multiply, add, cos, sin } = Animated;
 
 export const translateZ = (
   perspective: Animated.Adaptable<number>,
@@ -25,3 +25,16 @@ export const transformOrigin = (
   { translateX: multiply(x, -1) },
   { translateY: multiply(y, -1) },
 ];
+
+export const rotateTranslation = (
+  tr: Vector,
+  rotate: Animated.Adaptable<number>
+) => ({
+  x: sub(multiply(tr.x, cos(rotate)), multiply(tr.y, sin(rotate))),
+  y: add(multiply(tr.x, sin(rotate)), multiply(tr.y, cos(rotate))),
+});
+
+export const scaleTranslation = (
+  tr: Vector,
+  scale: Animated.Adaptable<number>
+) => vec.multiply(tr, scale);
