4c495cee3702c6cfd330b9dec316d1e8ae9f0607
William Candillon <wcandillon@gmail.com>
2020-04-27 07:29:16 +0200

    fix(🐛): fix bugs in atan2() (#244)

--- packages/core/src/__tests__/Math.test.ts
+++ packages/core/src/__tests__/Math.test.ts
@@ -7,6 +7,12 @@ test("atan2()", () => {
   expect(atan2(0, 0)[" __value"]).toBe(Math.atan2(0, 0));
   expect(atan2(0, 100)[" __value"]).toBe(Math.atan2(0, 100));
   expect(atan2(100, 0)[" __value"]).toBe(Math.atan2(100, 0));
+  expect(atan2(50, 25)[" __value"]).toBe(Math.atan2(50, 25));
+  expect(atan2(25, 50)[" __value"]).toBe(Math.atan2(25, 50));
+  expect(atan2(-1, 0)[" __value"]).toBe(Math.atan2(-1, 0));
+  expect(atan2(-3, -4)[" __value"]).toBe(Math.atan2(-3, -4));
+  expect(atan2(0, 3)[" __value"]).toBe(Math.atan2(0, 3));
+  expect(atan2(0, -3)[" __value"]).toBe(Math.atan2(0, -3));
 });
 
 test("round()", () => {
