0fb465fb224d19df6dd2d8688a43f256e33767be
William Candillon <wcandillon@gmail.com>
2020-05-05 10:08:39 +0200

    :wrench:

--- packages/core/src/__tests__/Matrix.test.ts
+++ packages/core/src/__tests__/Matrix.test.ts
@@ -13,17 +13,18 @@ test("accumulatedTransform()", () => {
   const tr = vec.create(10, 10);
   const origin = vec.create(5, 5);
   const scale = 2.3;
-  const { translateX, translateY, scale: scaleOffset } = decompose2d(
-    processTransform2d([
-      { translateX: tr.x },
-      { translateY: tr.y },
-      { translateX: origin.x },
-      { translateY: origin.y },
-      { scale },
-      { translateX: -origin.x },
-      { translateY: -origin.y },
-    ])
-  );
+  const transform = decompose2d([
+    { translateX: tr.x },
+    { translateY: tr.y },
+    { translateX: origin.x },
+    { translateY: origin.y },
+    { scale },
+    { translateX: -origin.x },
+    { translateY: -origin.y },
+  ]);
+  const { translateX } = transform[0];
+  const { translateY } = transform[1];
+  const { scaleX } = transform[3];
   expect(translateX[" __value"]).toBe(10 + 5 - 5 * scale);
   expect(translateY[" __value"]).toBe(10 + 5 - 5 * scale);
   const t = vec.add(
@@ -33,32 +34,41 @@ test("accumulatedTransform()", () => {
   );
   expect(translateX[" __value"]).toBe(t.x[" __value"]);
   expect(translateY[" __value"]).toBe(t.y[" __value"]);
-  expect(scaleOffset[" __value"]).toBe(2.3);
+  expect(scaleX[" __value"]).toBe(2.3);
 });
 
 test("accumulatedTransform() 1", () => {
   const tr = vec.create(10, 10);
   const origin = vec.create(5, 5);
-  const { translateX, translateY, scale } = decompose2d(
-    processTransform2d([
-      ...translate(tr),
-      ...transformOrigin(origin, { scale: 2.3 }),
-    ])
-  );
+  const transform = decompose2d([
+    ...translate(tr),
+    ...transformOrigin(origin, { scale: 2.3 }),
+  ]);
+  const { translateX } = transform[0];
+  const { translateY } = transform[1];
+  const { scaleX } = transform[3];
+  const { scaleY } = transform[4];
   expect(translateX[" __value"]).toBe(10 + 5 - 5 * 2.3);
   expect(translateY[" __value"]).toBe(10 + 5 - 5 * 2.3);
-  expect(scale[" __value"]).toBe(2.3);
+  expect(scaleX[" __value"]).toBe(2.3);
+  expect(scaleY[" __value"]).toBe(2.3);
 });
 
 test("accumulatedTransform() 2", () => {
   const tr = vec.create(10, 10);
   const origin = vec.create(-5, 0);
-  const { translateX, translateY, scale, rotateZ, skewX } = decompose2d(
+  const transform = decompose2d(
     processTransform2d([
       ...translate(tr),
       ...transformOrigin(origin, { rotateZ: Math.PI / 6 }),
     ])
   );
+  const { translateX } = transform[0];
+  const { translateY } = transform[1];
+  const { rotateZ: skewX } = transform[2];
+  const { scaleX } = transform[3];
+  const { scaleY } = transform[4];
+  const { rotateZ } = transform[5];
   expect(translateX[" __value"]).toBe(
     tr.x +
       origin.x +
@@ -76,21 +86,29 @@ test("accumulatedTransform() 2", () => {
   );
   expect(translateX[" __value"]).toBe(t.x[" __value"]);
   expect(t.y[" __value"]);
-  expect(scale[" __value"]).toBe(1);
+  expect(scaleX[" __value"]).toBe(1);
+  expect(scaleY[" __value"]).toBe(1);
   expect(rotateZ[" __value"] + skewX[" __value"]).toBeCloseTo(Math.PI / 6, 15);
 });
 
 test("accumulatedTransform() 3", () => {
   const tr = vec.create(10, 10);
   const origin = vec.create(0, 0);
-  const { translateX, translateY, scale, skewX, rotateZ } = decompose2d(
+  const transform = decompose2d(
     processTransform2d([
       ...translate(tr),
       ...transformOrigin(origin, { rotateZ: Math.PI / 6 }),
     ])
   );
+  const { translateX } = transform[0];
+  const { translateY } = transform[1];
+  const { rotateZ: skewX } = transform[2];
+  const { scaleX } = transform[3];
+  const { scaleY } = transform[4];
+  const { rotateZ } = transform[5];
   expect(translateX[" __value"]).toBe(tr.x);
   expect(translateY[" __value"]).toBe(tr.y);
-  expect(scale[" __value"]).toBe(1);
+  expect(scaleX[" __value"]).toBe(1);
+  expect(scaleY[" __value"]).toBe(1);
   expect(rotateZ[" __value"] + skewX[" __value"]).toBeCloseTo(Math.PI / 6, 15);
 });
