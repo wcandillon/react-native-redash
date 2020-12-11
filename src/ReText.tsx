610e7dc28b74cd452dde180282773804d979be5f
William Candillon <wcandillon@gmail.com>
2020-12-11 10:26:08 +0100

    fix(🐛): fixes #399

--- src/ReText.tsx
+++ src/ReText.tsx
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     color: "black",
   },
 });
+Animated.addWhitelistedNativeProps({ text: true });
 
 interface TextProps {
   text: Animated.SharedValue<string>;
