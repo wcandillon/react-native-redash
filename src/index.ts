1b75519cfd63165f253ea4c0bdf3ec0bb17fb0b9
William Candillon <wcandillon@gmail.com>
2020-12-11 10:39:03 +0100

    :wrench:

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from "./Coordinates";
 export * from "./Transitions";
 export * from "./Math";
 export * from "./Vectors";
-export * from "./Colors";
 export * from "./Paths";
 export * from "./Physics";
 export * from "./Array";
