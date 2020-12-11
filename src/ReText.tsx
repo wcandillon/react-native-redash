28888afad52dc6b0751a543ac06d495525c9cdc1
William Candillon <wcandillon@gmail.com>
2020-12-11 10:27:16 +0100

    :lipstick:

--- src/ReText.tsx
+++ src/ReText.tsx
@@ -1,7 +1,11 @@
-import * as React from "react";
-import { TextStyle, TextProps as RNTextProps, StyleSheet } from "react-native";
+import React from "react";
+import {
+  TextStyle,
+  TextProps as RNTextProps,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import Animated, { useAnimatedProps } from "react-native-reanimated";
-import { TextInput } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   baseStyle: {
