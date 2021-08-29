179b507f3c493f031ad007e1c034151d627494d8
William Candillon <wcandillon@gmail.com>
2021-08-29 12:45:24 +0200

    Add transform2d

--- src/Transforms.ts
+++ src/Transforms.ts
@@ -1,6 +1,7 @@
 import type { TransformsStyle } from "react-native";
 
 import type { Vector } from "./Vectors";
+import type { Transforms2d } from "./Matrix3";
 
 type RNTransform = Exclude<TransformsStyle["transform"], undefined>;
 
@@ -8,6 +9,20 @@ export const transformOrigin = (
   { x, y }: Vector,
   transformations: RNTransform
 ): RNTransform => {
+  "worklet";
+  return [
+    { translateX: x },
+    { translateY: y },
+    ...transformations,
+    { translateX: -x },
+    { translateY: -y },
+  ];
+};
+
+export const transform2d = (
+  { x, y }: Vector,
+  transformations: Transforms2d
+): Transforms2d => {
   "worklet";
   return [
     { translateX: x },
