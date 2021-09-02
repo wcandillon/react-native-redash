7af1141d6a80ecf8592e8a0b8bb9942cb27fc1a8
William Candillon <wcandillon@gmail.com>
2021-09-02 11:04:45 +0200

    :green_heart:

--- src/Colors.ts
+++ src/Colors.ts
@@ -60,21 +60,21 @@ export const hsv2rgb = (h: number, s: number, v: number) => {
 };
 
 export const opacity = (c: number): number => {
-  'worklet';
+  "worklet";
   return ((c >> 24) & 255) / 255;
 };
 
 export const red = (c: number): number => {
-  'worklet';
+  "worklet";
   return (c >> 16) & 255;
 };
 
 export const green = (c: number): number => {
-  'worklet';
+  "worklet";
   return (c >> 8) & 255;
 };
 
 export const blue = (c: number): number => {
-  'worklet';
+  "worklet";
   return c & 255;
 };
