267ad828227b44024137cf36e1072973c6d0bf6e
Mo Gorhom <gorhom@me.com>
2021-10-17 15:42:47 +0100

    feat: added threshold to snapPoint

--- src/Physics.ts
+++ src/Physics.ts
@@ -5,11 +5,27 @@
 export const snapPoint = (
   value: number,
   velocity: number,
-  points: ReadonlyArray<number>
+  points: ReadonlyArray<number>,
+  threshold = 0.5
 ): number => {
   "worklet";
   const point = value + 0.2 * velocity;
-  const deltas = points.map((p) => Math.abs(point - p));
-  const minDelta = Math.min.apply(null, deltas);
-  return points.filter((p) => Math.abs(point - p) === minDelta)[0];
+
+  let startPointIndex = 0;
+  let endPointIndex = points.length - 1;
+
+  for (let i = 0, k = points.length - 1; i < points.length; i++, k--) {
+    if (points[i] < point) {
+      startPointIndex = i;
+    }
+    if (points[k] > point && points[k] < points[endPointIndex]) {
+      endPointIndex = k;
+    }
+  }
+
+  const middlePoint =
+    points[startPointIndex] +
+    (points[endPointIndex] - points[startPointIndex]) * threshold;
+
+  return point < middlePoint ? points[startPointIndex] : points[endPointIndex];
 };
