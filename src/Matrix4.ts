ff0640d3befe88ddadee608fce0229579b5b600e
William Candillon <wcandillon@gmail.com>
2022-06-29 20:01:09 +0200

    Refactor Matrix4

--- src/Matrix4.ts
+++ src/Matrix4.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 import type { Matrix3 } from "./Matrix3";
 
 export type Vec3 = readonly [number, number, number];
@@ -75,55 +76,23 @@ export const identity4: Matrix4 = [
 /**
  * @worklet
  */
-const translateXMatrix = (x: number): Matrix4 => {
+const translate = (x: number, y: number, z: number): Matrix4 => {
   "worklet";
-  return [1, 0, 0, x, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1];
 };
 
 /**
  * @worklet
  */
-const translateYMatrix = (y: number): Matrix4 => {
+const scale = (sx: number, sy: number, sz: number): Matrix4 => {
   "worklet";
-  return [1, 0, 0, 0, 0, 1, 0, y, 0, 0, 1, 0, 0, 0, 0, 1];
+  return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
 };
 
 /**
  * @worklet
  */
-const translateZMatrix = (z: number): Matrix4 => {
-  "worklet";
-  return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, z, 0, 0, 0, 1];
-};
-
-/**
- * @worklet
- */
-const scaleMatrix = (s: number): Matrix4 => {
-  "worklet";
-  return [s, 0, 0, 0, 0, s, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-};
-
-/**
- * @worklet
- */
-const scaleXMatrix = (s: number): Matrix4 => {
-  "worklet";
-  return [s, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-};
-
-/**
- * @worklet
- */
-const skewXMatrix = (s: number): Matrix4 => {
-  "worklet";
-  return [1, Math.tan(s), 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-};
-
-/**
- * @worklet
- */
-const skewYMatrix = (s: number): Matrix4 => {
+const skewX = (s: number): Matrix4 => {
   "worklet";
   return [1, 0, 0, 0, Math.tan(s), 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 };
@@ -131,15 +100,15 @@ const skewYMatrix = (s: number): Matrix4 => {
 /**
  * @worklet
  */
-const scaleYMatrix = (s: number): Matrix4 => {
+const skewY = (s: number): Matrix4 => {
   "worklet";
-  return [1, 0, 0, 0, 0, s, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  return [1, Math.tan(s), 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 };
 
 /**
  * @worklet
  */
-const perspectiveMatrix = (p: number): Matrix4 => {
+const perspective = (p: number): Matrix4 => {
   "worklet";
   return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -1 / p, 1];
 };
@@ -147,7 +116,7 @@ const perspectiveMatrix = (p: number): Matrix4 => {
 /**
  * @worklet
  */
-const rotateXMatrix = (r: number): Matrix4 => {
+const rotateX = (r: number): Matrix4 => {
   "worklet";
   return [
     1,
@@ -156,10 +125,10 @@ const rotateXMatrix = (r: number): Matrix4 => {
     0,
     0,
     Math.cos(r),
-    -Math.sin(r),
-    0,
-    0,
     Math.sin(r),
+    0,
+    0,
+    -Math.sin(r),
     Math.cos(r),
     0,
     0,
@@ -172,18 +141,18 @@ const rotateXMatrix = (r: number): Matrix4 => {
 /**
  * @worklet
  */
-const rotateYMatrix = (r: number): Matrix4 => {
+const rotateY = (r: number): Matrix4 => {
   "worklet";
   return [
     Math.cos(r),
     0,
-    Math.sin(r),
+    -Math.sin(r),
     0,
     0,
     1,
     0,
     0,
-    -Math.sin(r),
+    Math.sin(r),
     0,
     Math.cos(r),
     0,
@@ -197,14 +166,14 @@ const rotateYMatrix = (r: number): Matrix4 => {
 /**
  * @worklet
  */
-const rotateZMatrix = (r: number): Matrix4 => {
+const rotateZ = (r: number): Matrix4 => {
   "worklet";
   return [
     Math.cos(r),
-    -Math.sin(r),
-    0,
-    0,
     Math.sin(r),
+    0,
+    0,
+    -Math.sin(r),
     Math.cos(r),
     0,
     0,
@@ -217,14 +186,6 @@ const rotateZMatrix = (r: number): Matrix4 => {
     0,
     1,
   ];
-};
-
-/**
- * @worklet
- */
-export const dot4 = (row: Vec4, col: Vec4) => {
-  "worklet";
-  return row[0] * col[0] + row[1] * col[1] + row[2] * col[2] + row[3] * col[3];
 };
 
 /**
@@ -232,11 +193,13 @@ export const dot4 = (row: Vec4, col: Vec4) => {
  */
 export const matrixVecMul4 = (m: Matrix4, v: Vec4) => {
   "worklet";
-  const row0 = [m[0], m[1], m[2], m[3]] as const;
-  const row1 = [m[4], m[5], m[6], m[7]] as const;
-  const row2 = [m[8], m[9], m[10], m[11]] as const;
-  const row3 = [m[12], m[13], m[14], m[15]] as const;
-  return [dot4(row0, v), dot4(row1, v), dot4(row2, v), dot4(row3, v)] as const;
+  const [vx, vy, vz, vw] = v;
+  return [
+    vx * m[0] + vy * m[4] + vz * m[8] + vw * m[12],
+    vx * m[1] + vy * m[5] + vz * m[9] + vw * m[13],
+    vx * m[2] + vy * m[6] + vz * m[10] + vw * m[14],
+    vx * m[3] + vy * m[7] + vz * m[11] + vw * m[15],
+  ];
 };
 
 /**
@@ -251,34 +214,62 @@ export const mapPoint3d = (m: Matrix4, v: Vec3) => {
 /**
  * @worklet
  */
-export const multiply4 = (m1: Matrix4, m2: Matrix4) => {
+export const multiply4 = (a: Matrix4, b: Matrix4) => {
   "worklet";
-  const col0 = [m2[0], m2[4 + 0], m2[8 + 0], m2[12 + 0]] as const;
-  const col1 = [m2[1], m2[4 + 1], m2[8 + 1], m2[12 + 1]] as const;
-  const col2 = [m2[2], m2[4 + 2], m2[8 + 2], m2[12 + 2]] as const;
-  const col3 = [m2[3], m2[4 + 3], m2[8 + 3], m2[12 + 3]] as const;
-  const row0 = [m1[0], m1[1], m1[2], m1[3]] as const;
-  const row1 = [m1[4], m1[5], m1[6], m1[7]] as const;
-  const row2 = [m1[8], m1[9], m1[10], m1[11]] as const;
-  const row3 = [m1[12], m1[13], m1[14], m1[15]] as const;
-  return [
-    dot4(row0, col0),
-    dot4(row0, col1),
-    dot4(row0, col2),
-    dot4(row0, col3),
-    dot4(row1, col0),
-    dot4(row1, col1),
-    dot4(row1, col2),
-    dot4(row1, col3),
-    dot4(row2, col0),
-    dot4(row2, col1),
-    dot4(row2, col2),
-    dot4(row2, col3),
-    dot4(row3, col0),
-    dot4(row3, col1),
-    dot4(row3, col2),
-    dot4(row3, col3),
-  ] as const;
+  const out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const a00 = a[0],
+    a01 = a[1],
+    a02 = a[2],
+    a03 = a[3],
+    a10 = a[4],
+    a11 = a[5],
+    a12 = a[6],
+    a13 = a[7],
+    a20 = a[8],
+    a21 = a[9],
+    a22 = a[10],
+    a23 = a[11],
+    a30 = a[12],
+    a31 = a[13],
+    a32 = a[14],
+    a33 = a[15];
+
+  let b0 = b[0],
+    b1 = b[1],
+    b2 = b[2],
+    b3 = b[3];
+  out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+  out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+  out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+  out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+  b0 = b[4];
+  b1 = b[5];
+  b2 = b[6];
+  b3 = b[7];
+  out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+  out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+  out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+  out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+  b0 = b[8];
+  b1 = b[9];
+  b2 = b[10];
+  b3 = b[11];
+  out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+  out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+  out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+  out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+
+  b0 = b[12];
+  b1 = b[13];
+  b2 = b[14];
+  b3 = b[15];
+  out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+  out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+  out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+  out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+  return out as unknown as Matrix4;
 };
 
 /**
@@ -286,7 +277,7 @@ export const multiply4 = (m1: Matrix4, m2: Matrix4) => {
  */
 export const toMatrix3 = (m: Matrix4): Matrix3 => {
   "worklet";
-  return [m[0], m[1], m[3], m[4], m[5], m[7], m[12], m[13], m[15]];
+  return [m[0], m[4], m[12], m[1], m[5], m[13], m[3], m[7], m[15]];
 };
 
 /**
@@ -298,51 +289,51 @@ export const processTransform3d = (transforms: Transforms3d) => {
     const key = Object.keys(transform)[0] as Transform3dName;
     if (key === "translateX") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, translateXMatrix(value));
+      return multiply4(acc, translate(value, 0, 0));
     }
     if (key === "translateY") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, translateYMatrix(value));
+      return multiply4(acc, translate(0, value, 0));
     }
     if (key === "translateZ") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, translateZMatrix(value));
+      return multiply4(acc, translate(0, 0, value));
     }
     if (key === "scale") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, scaleMatrix(value));
+      return multiply4(acc, scale(value, value, 1));
     }
     if (key === "scaleX") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, scaleXMatrix(value));
+      return multiply4(acc, scale(value, 1, 1));
     }
     if (key === "scaleY") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, scaleYMatrix(value));
+      return multiply4(acc, scale(1, value, 1));
     }
     if (key === "skewX") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, skewXMatrix(value));
+      return multiply4(acc, skewX(value));
     }
     if (key === "skewY") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, skewYMatrix(value));
+      return multiply4(acc, skewY(value));
     }
     if (key === "rotateX") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, rotateXMatrix(value));
+      return multiply4(acc, rotateX(value));
     }
     if (key === "rotateY") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, rotateYMatrix(value));
+      return multiply4(acc, rotateY(value));
     }
     if (key === "perspective") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, perspectiveMatrix(value));
+      return multiply4(acc, perspective(value));
     }
     if (key === "rotate" || key === "rotateZ") {
       const value = (transform as Pick<Transformations, typeof key>)[key];
-      return multiply4(acc, rotateZMatrix(value));
+      return multiply4(acc, rotateZ(value));
     }
     if (key === "matrix") {
       const matrix = (transform as Pick<Transformations, typeof key>)[key];
@@ -358,4 +349,12 @@ export const processTransform3d = (transforms: Transforms3d) => {
 export const concat4 = (m: Matrix4, transform: Transforms3d) => {
   "worklet";
   return multiply4(m, processTransform3d(transform));
+};
+
+export const Matrix4 = {
+  translate,
+  scale,
+  rotateX,
+  rotateY,
+  rotateZ,
 };
