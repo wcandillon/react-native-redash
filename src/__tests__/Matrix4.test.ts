5cff0da6c6a5e54a2063e18725e3f98dd2dbe266
William Candillon <wcandillon@gmail.com>
2023-12-09 11:51:05 +0100

    fix(🐛): fix bugs in the 4x4 matrix handling (#519)

--- src/__tests__/Matrix4.test.ts
+++ src/__tests__/Matrix4.test.ts
@@ -1,11 +1,21 @@
 import {
   concat4,
-  multiply4,
   identity4,
-  matrixVecMul4,
-  processTransform3d,
   mapPoint3d,
+  matrixVecMul4,
+  multiply4,
+  processTransform3d,
 } from "../Matrix4";
+
+import {
+  identity,
+  rotated,
+  multiply,
+  transformPoint,
+  translated,
+  scaled,
+  transformPoint3d,
+} from "./matrix";
 
 const expectArrayCloseTo = (
   a: readonly number[],
@@ -19,54 +29,57 @@ const expectArrayCloseTo = (
 };
 
 test("processTransform3d()", () => {
+  let ref = identity();
+  ref = multiply(ref, rotated([1, 0, 0], Math.PI));
+  ref = multiply(ref, rotated([0, 1, 0], Math.PI));
   expect(
     processTransform3d([{ rotateX: Math.PI }, { rotateY: Math.PI }])
-  ).toStrictEqual([
-    -1, 1.4997597826618576e-32, 1.2246467991473532e-16, 0, 0, -1,
-    1.2246467991473532e-16, 0, 1.2246467991473532e-16, 1.2246467991473532e-16,
-    1, 0, 0, 0, 0, 1,
-  ]);
+  ).toStrictEqual(ref);
 });
 
 test("multiply4()", () => {
+  let ref = identity();
+  ref = multiply(ref, rotated([1, 0, 0], Math.PI));
+  ref = multiply(ref, rotated([0, 1, 0], Math.PI));
   expect(
     multiply4(
       identity4,
       processTransform3d([{ rotateX: Math.PI }, { rotateY: Math.PI }])
     )
-  ).toStrictEqual([
-    -1, 1.4997597826618576e-32, 1.2246467991473532e-16, 0, 0, -1,
-    1.2246467991473532e-16, 0, 1.2246467991473532e-16, 1.2246467991473532e-16,
-    1, 0, 0, 0, 0, 1,
-  ]);
+  ).toStrictEqual(ref);
 });
 
 test("matrixVecMul4()", () => {
+  let ref = identity();
+  ref = multiply(ref, rotated([1, 0, 0], Math.PI));
+  ref = multiply(ref, rotated([0, 1, 0], Math.PI));
+  const refVec = transformPoint(ref, [0.5, 0.5, 0, 1]);
   expect(
     matrixVecMul4(
       processTransform3d([{ rotateX: Math.PI }, { rotateY: Math.PI }]),
       [0.5, 0.5, 0, 1]
     )
-  ).toStrictEqual([-0.5, -0.5, 1.2246467991473532e-16, 1]);
+  ).toStrictEqual(refVec);
 });
 
 describe("4x4 matrices", () => {
   it("can make a translated 4x4 matrix", () => {
+    let ref = identity();
+    ref = multiply(ref, translated([5, 6, 7]));
     expectArrayCloseTo(
       processTransform3d([
         { translateX: 5 },
         { translateY: 6 },
         { translateZ: 7 },
       ]),
-      [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 5, 6, 7, 1]
+      ref
     );
   });
 
   it("can make a scaled 4x4 matrix", () => {
-    expectArrayCloseTo(
-      processTransform3d([{ scaleX: 5 }, { scaleY: 6 }]),
-      [5, 0, 0, 0, 0, 6, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-    );
+    let ref = identity();
+    ref = multiply(ref, scaled([5, 6, 1]));
+    expectArrayCloseTo(processTransform3d([{ scaleX: 5 }, { scaleY: 6 }]), ref);
   });
 
   it("can multiply 4x4 matrices", () => {
@@ -78,30 +91,37 @@ describe("4x4 matrices", () => {
       2.0, 3.0, 4.0, 5.0, -3.0, -4.0, -5.0, -6.0, 7.0, 8.0, 9.0, 10.0, -4.0,
       -3.0, -2.0, -1.0,
     ] as const;
-    const expected = [
-      0.8000000000000003, -3.9000000000000004, -2.5000000000000004,
-      -1.0999999999999996, -1.2000000000000002, 4.5, 2.7000000000000006,
-      0.8999999999999995, 2.799999999999999, -6.8999999999999995,
-      -3.500000000000001, -0.09999999999999876, -1.6, -0.2999999999999996,
-      -1.2999999999999998, -2.3000000000000003,
-    ] as const;
-    expect(multiply4(a, b)).toEqual(expected);
+    let ref = identity();
+    ref = multiply(a, b);
+    expect(multiply4(a, b)).toEqual(ref);
   });
 
   it("maps 3D points correctly with a 3x3 matrix", () => {
+    // 1.
+    let ref = identity();
+    ref = multiply(ref, translated([10, 0, 0]));
+    ref = multiply(ref, scaled([2, 2, 1]));
+    ref = multiply(ref, translated([20, 0, 10]));
+    let refVec = transformPoint3d(ref, [0, 0, 10]);
     const a = processTransform3d([
       { translateX: 10 },
       { scale: 2 },
       { translateX: 20 },
       { translateZ: 10 },
     ]);
-    expect(mapPoint3d(a, [0, 0, 10])).toEqual([50, 0, 20]);
+    expect(a).toEqual(ref);
+    expect(mapPoint3d(a, [0, 0, 10])).toEqual(refVec);
+    // 2.
+    ref = identity();
+    ref = multiply(ref, translated([10, 20, 30]));
+    refVec = transformPoint3d(ref, [0, 0, 0]);
     const b = processTransform3d([
       { translateX: 10 },
       { translateY: 20 },
       { translateZ: 30 },
     ]);
-    expect(mapPoint3d(b, [0, 0, 0])).toEqual([10, 20, 30]);
+    expect(mapPoint3d(b, [0, 0, 0])).toEqual(refVec);
+    // 3.
     expect(
       concat4(
         concat4(b, [
